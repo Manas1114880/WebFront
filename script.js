6ae01fbdf6ab8bca8d506642a5428a5920e8b70f
Israel Emmanuel <sriemmanuel787@outlook.com>
2023-05-23 10:20:51 -0700

    Fixed layout in HTML and piece appearance

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class Square {
-	constructor(x, y, board, piece) {
-		this.board = board;
+	constructor(x, y, plane, piece) {
+		this.plane = plane;
 		this.x = x
 		this.y = y;
 		this.piece = piece;
@@ -8,15 +8,12 @@ class Square {
 
 		this.elem = document.createElement("div");
 		this.elem.classList.add("square");
-		console.log(this.board.elem);
-		this.board.elem.append(this.elem);
+		this.plane.elem.append(this.elem);
 		this.elem.addEventListener("click", this.placeMove);
-		console.log("I build a square");
 	}
 	placeMove() {
 		alert("I made a move");
-		this.piece = new Piece(new Square(0, 0, board, ""), "square", "white", board);
-		console.log(this.piece);
+		this.piece = new Piece(this, "square", "white", board);
 		this.piece.displayPiece();
 	}
 }
@@ -29,45 +26,45 @@ class Piece {
 		this.id = "P_" + team + "_" + type + "_" + this.square.x
 			+ "_" + this.square.y;
 
-		this.elem = document.createElement("div");
+		this.elem = document.createElement("img");
 		this.elem.classList.add("piece");
-
 	}
 	displayPiece() {
 		alert("Piece has moved");
-		this.square.elem.append(this.elem);
-		this.elem.classList.add("mickyMouse");
+		this.square.append(this.elem);
+		this.elem.src = "images.png";
 	}
 }
 
 class Board {
-	constructor(plane) {
-		// TDOD: Board's need an element if im going going to put new elements into it
-		this.plane = plane;
+	constructor() {
+		const size = 4;
+		this.planes = new Array(size);
 		this.elem = document.createElement("div");
 		this.elem.classList.add("board");
-		for (var i = 0; i < 4; i++) {
-			this.plane = new Plane(Piece);
+		document.body.append(this.elem);
+		for (var i = 0; i < this.planes.length; i++) {
+			this.planes[i] = new Plane(this);
 		}
 	}
 }
 
 class Plane {
-	constructor(pieces) {
+	constructor(board) {
+		this.board = board;
 		const size = 4;
 		this.size = size;
-		this.pieces = pieces;
-		this.elem = document.createElement("main");
-		document.querySelector(":root").style.setProperty("--xy", size);
-		this.elem.classList.add("board");
-		this.elem.setAttribute("style", "grid-template-columns: repeat(" + this.size + ",1fr);grid-template-rows: repeat(" + this.size + ",1fr);");
 		this.squares = new Array(size);
-		document.body.append(this.elem);
+		this.elem = document.createElement("div");
+		this.elem.classList.add("plane");
+		document.querySelector(":root").style.setProperty("--xy", size);
+		this.elem.setAttribute("style", "grid-template-columns: repeat(" + this.size + ",1fr);grid-template-rows: repeat(" + this.size + ",1fr);");
+		this.board.elem.append(this.elem);
 		this.drawSquares(this.size);
 	}
 	drawSquares(size) {
 		for (var x = 0; x < this.size; x++) {
-			this.squares[x] = new Array(size);
+			this.squares[x] = new Array(this.size);
 			for (var y = 0; y < this.size; y++) {
 				this.squares[x][y] = new Square(x, y, this);
 			}
@@ -75,9 +72,4 @@ class Plane {
 	}
 }
 
-////////////////////////////////////////////////
-//var square = new Square(1,1,"", "");
-//var piece = new Piece(square, "", "", "");
-//var board = new Board(Piece);
-var board = new Board(Plane);
-//console.log(square);
+var board = new Board();
